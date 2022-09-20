d130aee838d374a4cd3ddc06cc1385d29ba52d30
Jojoos1889 <113982420+Jojoos1889@users.noreply.github.com>
2022-09-20 11:41:19 +0200

    Add files via upload

---
+++ jojo os kernel.py
@@ -0,0 +1,97 @@
+#jojo os kernel v0.1 
+input2 = input (password:)
+if is not (input2 == 1889): exit
+if (input2 == 1889):
+cowsay jojo os: operative
+print("hello user this is anonymously os")
+print("jojo os version: gold experience")
+print("versione kernel: 0.1")
+print("state: root")
+input1 = input ("chose a fictures:1.hacking,2.music,3.internet,4.update")
+if (input1 == '1'):
+    print("[1]wireshark")
+    print("[2]aircrack-ng")
+    print("[3]sqlmap")
+    print("[4]nmap")
+    print("[5]commix")
+    print("[6]xsser(not avaiable)")
+    input3 = input ("~>"):
+        if (input3 == '1'):
+            input4 = input ("comando>"):
+            wireshark (input4)
+        if (input3 == '2'):
+            input5 = input ("comando>"):
+                aircrack-ng (input5)
+        if (input3 == '4'):
+            input7 = input ("comando>"):
+                nmap (input7)
+        if (input3 == '5'):
+            input8 = input ("comando>"):
+                commix (input8)
+if (input1 == '2'):
+    print("not aviable")
+if (input1 == '3'):
+    import sys
+from PyQt5.QtCore import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtWebEngineWidgets import *
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        self.browser = QWebEngineView()
+        self.browser.setUrl(QUrl('http://google.com'))
+        self.setCentralWidget(self.browser)
+        self.showMaximized()
+
+        # navbar
+        navbar = QToolBar()
+        self.addToolBar(navbar)
+
+        back_btn = QAction('Back', self)
+        back_btn.triggered.connect(self.browser.back)
+        navbar.addAction(back_btn)
+
+        forward_btn = QAction('Forward', self)
+        forward_btn.triggered.connect(self.browser.forward)
+        navbar.addAction(forward_btn)
+
+        reload_btn = QAction('Reload', self)
+        reload_btn.triggered.connect(self.browser.reload)
+        navbar.addAction(reload_btn)
+
+        home_btn = QAction('Home', self)
+        home_btn.triggered.connect(self.navigate_home)
+        navbar.addAction(home_btn)
+
+        self.url_bar = QLineEdit()
+        self.url_bar.returnPressed.connect(self.navigate_to_url)
+        navbar.addWidget(self.url_bar)
+
+        self.browser.urlChanged.connect(self.update_url)
+
+    def navigate_home(self):
+        self.browser.setUrl(QUrl('http://google.com'))
+
+    def navigate_to_url(self):
+        url = self.url_bar.text()
+        self.browser.setUrl(QUrl(url))
+
+    def update_url(self, q):
+        self.url_bar.setText(q.toString())
+app = QApplication(sys.argv)
+QApplication.setApplicationName('Safe Browser')
+window = MainWindow()
+app.exec_().
+if (input1 == '4'):
+    apt update wireshark
+    apt upgrade wireshark
+    apt update aircrack-ng
+    apt upgrade aircrack-ng
+    apt update sqlmap
+    apt upgrade sqlmap 
+    apt update nmap 
+    apt upgrade nmap 
+    apt update commix 
+    apt upgrade commix
+    print("finish updating all of pakets!")
+    print("resrart the kernel!")
---
+++ setup .py
@@ -0,0 +1,16 @@
+#jojo os e un sistema operativo da terminale
+print("setup of jojo os")
+print("made by au0_error")
+print("installing dependance...")
+print("this script must be run whit root")
+apt upgrade && apt update
+apt install wireshark 
+apt install aircrack-ng
+apt install cowsay 
+apt install ssh 
+apt install sqlmap 
+apt install nmap 
+apt install commix 
+print("done")
+print("youre passwd is 1889")
+cowsay bye!
