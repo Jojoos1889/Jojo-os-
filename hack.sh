d6572ca805780e6f5cce99030b634ab5a18a6b0c
Jojoos1889 <113982420+Jojoos1889@users.noreply.github.com>
2022-09-20 13:47:09 +0200

    Add files via upload

---
+++ hack.sh
@@ -0,0 +1,7 @@
+nmap -Pn (ip)
+commix -u http://ip --level=3
+sqlmap -u http://ip -D --dbms
+wireshark +commando
+xsser -u http://ip -c --Cl=3
+cd routersploit 
+python3 rsf.py
---
+++ setup.sh
@@ -0,0 +1,11 @@
+apt upgrade && apt update
+apt install wireshark 
+apt install aircrack-ng
+apt install cowsay 
+apt install ssh 
+apt install sqlmap 
+apt install nmap 
+apt install commix 
+git clone https://github.com/threat9/routersploit
+cd routersploit
+pip install -r requirements.txt
