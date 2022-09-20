cf3e805cd7705453d006b2d6c6a815fa575fa04e
Jojoos1889 <113982420+Jojoos1889@users.noreply.github.com>
2022-09-20 13:45:53 +0200

    Delete setup .py

--- setup .py
+++
@@ -1,9 +0,0 @@
-#jojo os e un sistema operativo da terminale
-print("setup of jojo os")
-print("made by au0_error")
-print("installing dependance...")
-print("this script must be run whit root")
-open("setup.sh")
-print("done")
-print("youre passwd is 1889")
-cowsay bye!
